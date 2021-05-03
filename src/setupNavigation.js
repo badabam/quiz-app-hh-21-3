1f6e2f981a82903ef72a053b2c9ca813980ec5be
Jeremias Erbs <jerry@badabam.de>
2021-05-03 15:26:06 +0200

    Cleanup and add header text logic

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,23 +1,41 @@
 import getAllElements from './utils/getAllElements'
+import getElement from './utils/getElement'
 
 export default function setupNavigation() {
   const pages = getAllElements('[data-page]')
   const buttons = getAllElements('[data-nav]')
+  const header = getElement('[data-header]')
 
-  buttons.forEach(button => {
+  buttons.forEach(setupButton)
+
+  function setupButton(button) {
     const buttonName = button.dataset.nav
-    button.addEventListener('click', () => {
-      pages.forEach(page => {
-        const pageName = page.dataset.page
-        page.classList.toggle('hidden', pageName !== buttonName)
-      })
+    const headerText = button.dataset.headerText
 
-      buttons.forEach(button => {
-        button.classList.toggle(
-          'navigation__link--active',
-          button.dataset.nav === buttonName
-        )
-      })
+    button.addEventListener('click', () => {
+      updateHeader(headerText)
+      changePage(buttonName)
+      updateNavigation(buttonName)
     })
-  })
+  }
+
+  function updateHeader(text) {
+    header.innerText = text
+  }
+
+  function updateNavigation(buttonName) {
+    buttons.forEach(button => {
+      button.classList.toggle(
+        'navigation__link--active',
+        button.dataset.nav === buttonName
+      )
+    })
+  }
+
+  function changePage(buttonName) {
+    pages.forEach(page => {
+      const pageName = page.dataset.page
+      page.classList.toggle('hidden', pageName !== buttonName)
+    })
+  }
 }
