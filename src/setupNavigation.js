b4c66f4ed36ef4c7251276f1d6d646719ecb9bd9
Jeremias Erbs <jerry@badabam.de>
2021-05-03 12:34:42 +0200

    Implement page change with forEach

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,57 +1,24 @@
-import getElement from './utils/getElement'
+import getAllElements from './utils/getAllElements'
 
 export default function setupNavigation() {
-  const buttonHome = getElement('.js-home-button')
-  const buttonBookmark = getElement('.js-bookmark-button')
-  const buttonCreate = getElement('.js-create-button')
+  const pages = getAllElements('[data-page]')
+  const buttons = getAllElements('[data-nav]')
 
-  const homePage = getElement('.js-page-home')
-  const bookmarkPage = getElement('.js-page-bookmark')
-  const createPage = getElement('.js-page-create')
+  buttons.forEach(button => {
+    const buttonName = button.dataset.nav
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.page
 
-  const homePageHeader = getElement('.js-page-home-header')
-  const bookmarkPageHeader = getElement('.js-page-bookmark-header')
-  const createPageHeader = getElement('.js-page-create-header')
+        page.classList.toggle('hidden', pageName !== buttonName)
 
-  buttonHome.addEventListener('click', () => {
-    homePage.classList.remove('hidden')
-    bookmarkPage.classList.add('hidden')
-    createPage.classList.add('hidden')
-
-    homePageHeader.classList.remove('hidden')
-    bookmarkPageHeader.classList.add('hidden')
-    createPageHeader.classList.add('hidden')
-
-    buttonHome.classList.add('navigation__link--active')
-    buttonBookmark.classList.remove('navigation__link--active')
-    buttonCreate.classList.remove('navigation__link--active')
-  })
-
-  buttonBookmark.addEventListener('click', () => {
-    homePage.classList.add('hidden')
-    bookmarkPage.classList.remove('hidden')
-    createPage.classList.add('hidden')
-
-    homePageHeader.classList.add('hidden')
-    bookmarkPageHeader.classList.remove('hidden')
-    createPageHeader.classList.add('hidden')
-
-    buttonHome.classList.remove('navigation__link--active')
-    buttonBookmark.classList.add('navigation__link--active')
-    buttonCreate.classList.remove('navigation__link--active')
-  })
-
-  buttonCreate.addEventListener('click', () => {
-    homePage.classList.add('hidden')
-    bookmarkPage.classList.add('hidden')
-    createPage.classList.remove('hidden')
-
-    homePageHeader.classList.add('hidden')
-    bookmarkPageHeader.classList.add('hidden')
-    createPageHeader.classList.remove('hidden')
-
-    buttonHome.classList.remove('navigation__link--active')
-    buttonBookmark.classList.remove('navigation__link--active')
-    buttonCreate.classList.add('navigation__link--active')
+        // same result as the line above:
+        // if (pageName === buttonName) {
+        //   page.classList.remove('hidden')
+        // } else {
+        //   page.classList.add('hidden')
+        // }
+      })
+    })
   })
 }
